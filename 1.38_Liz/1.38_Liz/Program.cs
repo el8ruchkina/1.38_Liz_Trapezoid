a7097cfb4f5bd641b36d4161308e9275a13c9a33
el8ruchkina <115945562+el8ruchkina@users.noreply.github.com>
2022-10-16 21:32:13 +0300

    Add files via upload

---
+++ 1.38_Liz/1.38_Liz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Mainclass
+{
+    public static void Main()
+    {
+        double a, b, h, c, p;
+        Console.WriteLine("Введите меньшее основание:");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите большее основание:");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите высоту:");
+        h = double.Parse(Console.ReadLine());
+        c = Math.Sqrt(Math.Pow((b - a) / 2, 2) + Math.Pow(h, 2));
+        p = (2 * c) + a + b;
+        Console.WriteLine($"Периметр трапеции равен {p}");
+    }
+}
